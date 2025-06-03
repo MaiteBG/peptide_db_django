e4b8a2950b229fb45e76f83170b9d897ba6b91db
Maite Bernaus <maite.bernaus@gmail.com>
2025-06-03 11:11:52 +0200

    Feat: Add "Show more" toggle for long References and Protein function text in ProteinsByOrganismView
    - Enhance get_queryset method to add truncated and full references text to each protein.
    - Update template to display truncated texts with a toggle link when text exceeds 50 characters.
    - Implement data attributes to facilitate JS toggling of full/truncated reference display.

--- peptide_project/proteins/views.py
+++ peptide_project/proteins/views.py
@@ -1,5 +1,4 @@
 from django.shortcuts import get_object_or_404
-from django.utils.text import slugify
 from django.views.generic import ListView
 
 from catalog.models import Organism
@@ -8,15 +7,23 @@ from proteins.models import Protein
 
 class ProteinsByOrganismView(ListView):
     model = Protein
-    template_name = "proteins/proteins_by_organism.html"  # o el path correcto
+    template_name = "proteins/proteins_by_organism.html"
     context_object_name = "proteins"
-
 
     def get_queryset(self):
         slug = self.kwargs["scientific_name"]
         scientific_name = slug.replace("-", " ")
         organism = get_object_or_404(Organism, scientific_name__iexact=scientific_name)
-        return Protein.objects.filter(organism=organism)
+        proteins = Protein.objects.filter(organism=organism).select_related('organism', 'sequence')
+
+        # Añadimos atributos extra para las referencias
+        for protein in proteins:
+            refs = protein.sequence.references.all()
+            refs_text = "\n".join(str(ref) for ref in refs)
+            protein.references_text = refs_text
+            protein.references_text_trunc = (refs_text[:50] + "…") if len(refs_text) > 50 else refs_text
+
+        return proteins
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
